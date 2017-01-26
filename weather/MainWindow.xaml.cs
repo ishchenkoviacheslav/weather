3e702b323f8fc1f83276bdcbb5ae027cade2663b
user <user@DESKTOP-05J4HLG>
2017-01-26 20:45:47 +0200

    response from weather in json

--- weather/MainWindow.xaml.cs
+++ weather/MainWindow.xaml.cs
@@ -12,7 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Web;
 namespace weather
 {
     /// <summary>
@@ -23,6 +25,35 @@ namespace weather
         public MainWindow()
         {
             InitializeComponent();
+            string apiStr = "http://api.openweathermap.org/data/2.5/weather?q=Poltava,ukr&appid=478b547e0a226c53095708abf9fe3a3a";
+            RootObject weatherData = MakeRequest(apiStr);
+            if(weatherData!=null)
+            {
+                this.DataContext = weatherData;
+            }
+
+        }
+        public static RootObject MakeRequest(string requestUrl)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest;
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        throw new Exception(String.Format("Server error (HTTP {0}: {1}).", response.StatusCode, response.StatusDescription));
+
+                    DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(RootObject));
+                    object objResponse = jsonSerializer.ReadObject(response.GetResponseStream());
+                    RootObject jsonResponse = objResponse as RootObject;
+                    return jsonResponse;
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
         }
     }
 }
