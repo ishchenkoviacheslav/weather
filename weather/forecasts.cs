6ba5beb2b3364ce249421109486373cfbdb175bd
user <user@DESKTOP-05J4HLG>
2017-01-30 22:35:59 +0200

    continue mvvm

---
+++ weather/forecasts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace weather
+{
+    class forecasts : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
