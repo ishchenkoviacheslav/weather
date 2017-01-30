bb28b391009d7716756ed016d373a9566a1cea5c
user <user@DESKTOP-05J4HLG>
2017-01-30 22:26:27 +0200

    thinking about MVVM

---
+++ weather/currentWeather.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace weather
+{
+    class currentWeather: INotifyPropertyChanged
+    {
+        private City currentCity;
+        public City CurrentCity
+        {
+            get
+            {
+                return currentCity;
+            }
+            set
+            {
+                currentCity = value;
+                OnPropertyChanged("CurrentCity");
+            }
+        }
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
